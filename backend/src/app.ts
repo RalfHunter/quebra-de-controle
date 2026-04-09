0c7b0c0862c2e3be263204651e102a41a7c0ba05
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2026-04-09 19:35:40 -0400

    adicionando helmet

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/index.ts"
+import helmet from 'helmet'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(helmet())
 
 app.use(router)
 
